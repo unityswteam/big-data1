c40d4823a3c1d15ee53f6a18288e3846d8e8498a
Abebaw-Addis <abadis1221@gmail.com>
2025-10-15 00:06:37 +0300

    Adding .env and .gitignore files

--- db_config.py
+++ db_config.py
@@ -1,18 +1,22 @@
+# Establish and return a PostgreSQL database connection.
+import os
+from dotenv import load_dotenv
 import psycopg2
 
-# Establish and return a PostgreSQL database connection.
+# Load environment variables from .env file
+load_dotenv()
+
 def get_connection():
     try:
         conn = psycopg2.connect(
-            dbname="Big_Data",
-            user="postgres",
-            password="postabnet",
-            host="localhost",
-            port="5432"
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
         )
         print("Database connection established successfully.")
         return conn
     except Exception as e:
-        print(" Database connection failed:", e)
+        print("Database connection failed:", e)
         raise
-    
