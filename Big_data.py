1c48c083878ff11dda22882dddee82e7ce023605
Daniel <danihailhabt321@gmail.com>
2025-10-15 10:49:10 -0900

    update Big_data.py

--- Big_data.py
+++ Big_data.py
@@ -11,24 +11,51 @@ cursor = conn.cursor()
 # create table (if not exists)
 create_table_query = f"""
 CREATE TABLE IF NOT EXISTS {table_name} (
-    transaction_id INT PRIMARY KEY,
-    date VARCHAR(20),
-    product VARCHAR(100),
-    category VARCHAR(50),
-    price FLOAT,
-    quantity INT,
-    total FLOAT,
-    payment_method VARCHAR(50),
-    country VARCHAR(50)
+    flow_id SERIAL PRIMARY KEY,
+    destination_port INT,
+    flow_duration BIGINT,
+    total_fwd_packets BIGINT,
+    total_length_of_fwd_packets BIGINT,
+    fwd_packet_length_max BIGINT,
+    fwd_packet_length_min BIGINT,
+    bwd_packet_length_max BIGINT,
+    bwd_packet_length_min BIGINT,
+    flow_bytes DOUBLE PRECISION,
+    flow_packets DOUBLE PRECISION,
+    flow_iat_mean DOUBLE PRECISION,
+    flow_iat_std DOUBLE PRECISION,
+    flow_iat_min BIGINT,
+    bwd_iat_total BIGINT,
+    bwd_iat_mean DOUBLE PRECISION,
+    bwd_iat_std DOUBLE PRECISION,
+    bwd_iat_max BIGINT,
+    fwd_psh_flags BIGINT,
+    fwd_urg_flags BIGINT,
+    bwd_packets DOUBLE PRECISION,
+    min_packet_length BIGINT,
+    fin_flag_count BIGINT,
+    rst_flag_count BIGINT,
+    psh_flag_count BIGINT,
+    ack_flag_count BIGINT,
+    urg_flag_count BIGINT,
+    downup_ratio BIGINT,
+    init_win_bytes_forward BIGINT,
+    init_win_bytes_backward BIGINT,
+    active_mean DOUBLE PRECISION,
+    active_std DOUBLE PRECISION,
+    active_max BIGINT,
+    idle_std DOUBLE PRECISION,
+    attack_type BIGINT
 );
 """
+
 cursor.execute(create_table_query)
 conn.commit()
 print(f"Table '{table_name}' created successfully.")
 
 
 # Load the file and Extract
-csv_path = "sales_transactions_3200000.csv"
+csv_path = 'cic_ids_2017.csv'
 chunk_size = 100000
 print("Reading full CSV in chunks...")
 
@@ -37,32 +64,51 @@ for chunk in pd.read_csv(csv_path, chunksize=chunk_size):
     chunks.append(chunk)
 df = pd.concat(chunks, ignore_index=True)
 
+print(df.info())
 before = len(df)
 print(f"Total rows before duplicated rows are removed: {before}")
 
-# Remove duplicates globally (no saving to file)
-df.drop_duplicates(inplace=True)
-after = len(df)
-print(f"Removed {before - after} duplicate rows globally. Remaining: {after}")
+# Finding missing_values for all features
 
-# Ensure total = price × quantity
-if {'price', 'quantity', 'total'}.issubset(df.columns):
-    df['total'] = df.apply(
-        lambda r: r['price'] * r['quantity'] if pd.isnull(r['total']) or r['total'] == 0 else r['total'],
-        axis=1
-    )
-    
-# Fill missing values, inconsistencies, and transformations
+missing_values = df.isnull().sum()
+missing_cols = []
+missing_values.sort_values(ascending=False).plot(kind='bar', figsize=(12, 6), color='tomato')
+# plt.title("Missing Values Per Feature")
+# plt.ylabel("Number of Missing Values")
+# plt.xticks(rotation=90)
+# plt.grid(True)
+# plt.show()
+
 for col in df.columns:
-    if df[col].isnull().sum() > 0:
-        if df[col].dtype in ["float64", "int64"]:
-            mean_value = df[col].mean()
-            df[col] = df[col].fillna(mean_value)
-            print(f"Filled missing numeric '{col}' with mean: {mean_value:.2f}")
+    if df[col].isnull().any():
+        print(f"Column '{col}' has missing values. {df[col].isnull().sum()} with {df[col].dtype} data_type")
+        missing_cols.append(col)
+
+# Missing values treatment
+
+if missing_values.sum() > 0:
+    print("Missing values found. Proceeding with imputation.")
+    for col in missing_cols:
+        if df[col].dtype == 'object':
+          df[col] = df[col].fillna(df[col].mode()[0])
         else:
-            mode_value = df[col].mode()[0] if not df[col].mode().empty else "Unknown"
-            df[col] = df[col].fillna(mode_value)
-            print(f"Filled missing text '{col}' with mode: {mode_value}")
+          df[col] = df[col].fillna(df[col].mean())
+else:
+    print("No missing values found.")
+
+# Check for duplicates for all dataset features
+duplicates = df[df.duplicated(keep=False)]
+if not duplicates.empty:
+    print(f"{len(duplicates)} duplicate rows found on the dataset")
+else:
+    print("\nNo duplicates found.")
+print(f"Current shape of the dataset: {df.shape}")
+
+# Remove duplicated rows if any
+if len(duplicates) > 0:
+    print(f"Removing {len(duplicates)} duplicated rows...")
+    df.drop_duplicates(inplace=True)
+    print(f"Current shape of the dataset: {df.shape}")
 
 # Load into postgreSQL in chunks
 chunk_size = 100000
@@ -70,15 +116,53 @@ print("Loading data into PostgreSQL...")
 
 insert_query = f"""
 INSERT INTO {table_name} (
-    transaction_id, date, product, category, price, quantity, total, payment_method, country
+    destination_port,
+    flow_duration,
+    total_fwd_packets,
+    total_length_of_fwd_packets,
+    fwd_packet_length_max,
+    fwd_packet_length_min,
+    bwd_packet_length_max,
+    bwd_packet_length_min,
+    flow_bytes,
+    flow_packets,
+    flow_iat_mean,
+    flow_iat_std,
+    flow_iat_min,
+    bwd_iat_total,
+    bwd_iat_mean,
+    bwd_iat_std,
+    bwd_iat_max,
+    fwd_psh_flags,
+    fwd_urg_flags,
+    bwd_packets,
+    min_packet_length,
+    fin_flag_count,
+    rst_flag_count,
+    psh_flag_count,
+    ack_flag_count,
+    urg_flag_count,
+    downup_ratio,
+    init_win_bytes_forward,
+    init_win_bytes_backward,
+    active_mean,
+    active_std,
+    active_max,
+    idle_std,
+    attack_type
 )
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-ON CONFLICT (transaction_id) DO NOTHING;
+VALUES (
+    %s, %s, %s, %s, %s, %s, %s, %s,
+    %s, %s, %s, %s, %s, %s, %s, %s,
+    %s, %s, %s, %s, %s, %s, %s, %s,
+    %s, %s, %s, %s, %s, %s, %s, %s,
+    %s, %s
+);
 """
 
 for i in range(0, len(df), chunk_size):
     chunk = df.iloc[i:i + chunk_size]
-    records = [tuple(x) for x in chunk.to_numpy()]
+    records = [tuple(x) for x in chunk.astype(object).to_numpy()]
     execute_batch(cursor, insert_query, records)
     conn.commit()
     print(f" Loaded chunk {i // chunk_size + 1} ({len(records)} rows) into PostgreSQL")
